10510971f5d2142992e85e234e4f5a9235550746
Tye <tyewalker09@gmail.com>
2021-07-13 20:21:18 +0200

    Fixed itemList.py

--- itemList.py
+++ itemList.py
@@ -1,29 +1,70 @@
-import mysql.connector
-import datetime, timeago
-import re
+# Barrows: : Layout: Head Body Legs Weapon Set
+Ahrim = [4708, 4712, 4714, 4710, 12881]
+Dharok = [4716, 4720, 4718, 4722, 12877]
+Guthan = [4724, 4728, 4730, 4726, 12873]
+Karil = [4732, 4736, 4738, 4734, 12883]
+Torag = [4745, 4749, 4751, 4747, 12879]
+Verac = [4753, 4757, 4759, 4755, 12875]
 
-# SQL Prepared Statement:
-select_set = ("SELECT A.itemID, A.itemName, B.itemHigh, B.itemHighTime, B.itemLow, B.itemLowTime "
-                "FROM item A, itemlatest B "
-                "WHERE A.itemID = B.itemID AND A.itemID = %s")
+Barrows = [Ahrim, Dharok, Guthan, Karil, Torag, Verac]
 
+# Gold-trimmed standard metal (lg) : Layout: Head Body Legs Shield Set
+Bronze_gt_lg = [12211, 12205, 12207, 12213, 12968]
+Iron_gt_lg = [12241, 12235, 12237, 12243, 12980]
+Steel_gt_lg = [20178, 20169, 20172, 20181, 20382]
+Black_gt_lg = [2595, 2591, 2593, 2597, 12996]
+Mithril_gt_lg = [12283, 12277, 12279, 12281, 13008]
+Adamant_gt_lg = [2613, 2607, 2609, 2611, 13020]
+Rune_gt_lg = [2619, 2615, 2617, 2621, 13032]
+Gilded_gt_lg = [3486, 3481, 3483, 3488, 13036]
 
-def internalLink(name):
-    new_name = re.sub('\'', "-", name)
-    final_name = re.sub(' ', '-', new_name)
-    return final_name.lower()
+Gold_trimmed_sm_lg = [Bronze_gt_lg, Iron_gt_lg, Steel_gt_lg, Black_gt_lg, Mithril_gt_lg, Adamant_gt_lg, Rune_gt_lg,
+                      Gilded_gt_lg]
 
+# Trimmed standard metal (lg) : Layout: Head Body Legs Shield Set
+Bronze_t_lg = [12221, 12215, 12217, 12223, 12964]
+Iron_t_lg = [12231, 12225, 12227, 12233, 12976]
+Steel_gt_t_lg = [20193, 20184, 20187, 20196, 20376]
+Black_gt_t_lg = [2587, 2583, 2585, 2589, 12992]
+Mithril_t_lg = [12293, 12287, 12289, 12291, 13004]
+Adamant_t_lg = [2605, 2599, 2601, 2603, 13016]
+Rune_t_lg = [2627, 2623, 2625, 2629, 13028]
 
-def homePage(myItem = [], *args):
-    all_items_tuple = tuple()
-    now = datetime.datetime.now()
-    connectDB = mysql.connector.connect(host="localhost", user="root", passwd="", database="osrsprices", port=3303)
-    cursor = connectDB.cursor()
-    for x in myItem:
-        select_tuple = (str(x[-1]), )
-        cursor.execute(select_set, select_tuple)
-        myresult = cursor.fetchone()
-        diff = int(myresult[2]) - int(myresult[4])
-        item_set_tuple = (myresult[0], str(myresult[0]) + ".png", myresult[1], myresult[2], timeago.format(myresult[3], now), myresult[4], timeago.format(myresult[5], now), internalLink(myresult[1]), diff)
-        all_items_tuple = all_items_tuple + (item_set_tuple,)
-    return all_items_tuple
+Trimmed_sm_lg = [Bronze_t_lg, Iron_t_lg, Steel_gt_t_lg, Black_gt_t_lg, Adamant_t_lg, Rune_t_lg]
+
+# Gold-trimmed standard metal (sk) : Layout: Head Body Skirt Shield Set
+Bronze_gt_sk = [12211, 12205, 12209, 12213, 12970]
+Iron_gt_sk = [12241, 12235, 12239, 12243, 12982]
+Steel_gt_sk = [20178, 20169, 20175, 20181, 20385]
+Black_gt_sk = [2595, 2591, 3473, 2597, 12998]
+Mithril_gt_sk = [12283, 12277, 12285, 12281, 13010]
+Adamant_gt_sk = [2613, 2607, 3475, 2611, 13022]
+Rune_gt_sk = [2619, 2615, 3476, 2621, 13034]
+Gilded_gt_sk = [3486, 3481, 3485, 3488, 13038]
+
+Gold_trimmed_sm_sk = [Bronze_gt_sk, Iron_gt_sk, Steel_gt_sk, Black_gt_sk, Mithril_gt_sk, Adamant_gt_sk, Rune_gt_sk,
+                      Gilded_gt_sk]
+
+# Trimmed standard metal (sk): Layout : Head Body Skirt Shield Set
+Bronze_t_sk = [12221, 12215, 12219, 12223, 12966]
+Iron_t_sk = [12231, 12225, 12229, 12233, 12978]
+Steel_gt_t_sk = [20193, 20184, 20190, 20196, 20379]
+Black_gt_t_sk = [2587, 2583, 3472, 2589, 12994]
+Mithril_t_sk = [12293, 12287, 12295, 12291, 13006]
+Adamant_t_sk = [2605, 2599, 3474, 2603, 13018]
+Rune_t_sk = [2627, 2623, 3477, 2629, 13030]
+
+Trimmed_sm_sk = [Bronze_t_sk, Iron_t_sk, Steel_gt_t_sk, Black_gt_t_sk, Mithril_t_sk, Adamant_t_sk, Rune_t_sk]
+
+# God-Blessed dragonhide: Layout : Coif Body Chaps Bracers Set
+guthix_dhide = [10382, 10378, 10380, 10376, 13165]
+saradomin_dhide = [10390, 10386, 10388, 10384, 13163]
+zamorak_dhide = [10374, 10370, 10372, 10368, 13161]
+ancient_dhide = [12496, 12492, 12494, 12490, 13171]
+armadyl_dhide = [12512, 12508, 12510, 12506, 13169]
+bandos_dhide = [12504, 12500, 12502, 12498, 13167]
+
+God_blessed_dhide = [guthix_dhide, saradomin_dhide, zamorak_dhide, ancient_dhide, armadyl_dhide, bandos_dhide]
+
+# All:
+all_sets = [Barrows, Gold_trimmed_sm_lg, Trimmed_sm_lg, Gold_trimmed_sm_sk, Trimmed_sm_sk, God_blessed_dhide]
